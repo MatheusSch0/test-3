263550d2d65519dffb9920ae9c884e5deed39bed
MatheusSch0 <144277969+MatheusSch0@users.noreply.github.com>
2023-09-14 16:58:24 -0300

    Add files via upload

---
+++ aula3_part2.py
@@ -0,0 +1,37 @@
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado
+day = "nublado e chuvoso"
+tempo = input("quer saber como esta o tempo? ")
+print('o dia esta:',day)
+
+
+# 6 - Crie três variáveis para armazenar a quantidade de horas, minutos e segundos. Concatene esses valores para formar uma mensagem de tempo no formato "hh:mm:ss".**
+hora = input('que horas são? ')
+minu = input('agora os minutos ')
+seg = input('não esqueca os segundos ')
+print("são",hora,":",minu,":",seg)
+
+# 7 - Declare duas variáveis com números de telefone, incluindo um código de área e o número principal. Concatene esses valores para formar um número de telefone completo.
+area = '011'
+numero = input("qual seu numero?")
+print('seu number é', area,numero)
+
+# 8 - Crie uma lista de ingredientes para uma receita. Use concatenação para formar uma única string que liste os ingredientes separados por vírgulas.
+rece = 'sorvete'
+ind1 = 'ninho'
+ind2 = 'leite cond'
+ind3 = 'creme'
+ind4 = 'leite'
+ind5 = "gelatina"
+
+print('para fazer o sorvete voce precisa:',ind1,",",ind2,",",ind3,",",ind4,",",ind5,",")
+
+# 9 - Peça ao usuário para digitar três adjetivos e armazene-os em variáveis. Em seguida, use essas palavras para criar uma frase concatenada que descreve algo interessante.
+dsd = input('diga uma caracteristica sua?')
+ad1 = input('mais uma por favor...')
+ioi = input('uau,e outra ')
+print('ta metendo do loko que voce é:',dsd,",",ad1,',',ioi)
+5
+2
+
+
+
---
+++ aula_3.py
@@ -0,0 +1,1 @@
+## Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
